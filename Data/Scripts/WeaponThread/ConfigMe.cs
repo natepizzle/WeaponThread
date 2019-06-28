6fd940bfbde32cbc91610e2697d046b0e667ef09
Shane Stixrud <shane@geeklords.org>
2019-06-27 21:58:36 -0700

    dsadas

--- Data/Scripts/WeaponThread/ConfigMe.cs
+++ Data/Scripts/WeaponThread/ConfigMe.cs
@@ -273,6 +273,135 @@ namespace WeaponThread
                     },
                 },
             },
+			      // Second Weapon part on PDCTurretLB turret
+            new WeaponDefinition
+            {
+                TurretDef = new TurretDefinition
+                {
+                    DefinitionId = "PhotonTorpedo",
+                    AmmoMagazineId = "Blank",
+                    TurretMode = false,
+                    TrackTarget = false,
+                    RateOfFire = 60,
+                    BarrelsPerShot = 1,
+                    SkipBarrels = 0,
+                    ShotsPerBarrel = 1,
+                    RotateSpeed = 0.05f,
+                    ElevationSpeed = 0.05f,
+                    ReloadTime = 6000,
+                    ReleaseTimeAfterFire = 204f,
+                    HeatPerRoF = 1,
+                    MaxHeat = 180,
+                    HeatSinkRate = 2,
+                    ShotEnergyCost = 0,
+
+                    MountPoints = new []
+                    {
+                        new MountPoint {SubtypeId = "LargeFixedPositionMissileTurret", SubpartId = "None"},
+                    },
+
+                    Barrels = new []
+                    {
+                        "muzzle_missile_001", "muzzle_missile_002",
+                    },
+
+                },
+                AmmoDef = new AmmoDefinition
+                {
+                    DefaultDamage = 0f,
+                    AreaEffectYield = 0f,
+                    AreaEffectRadius = 0f,
+                    DetonateOnEnd = false,
+                    ProjectileLength = 1f,
+                    Mass = 150f,  // in grams
+                    Health = 0f,
+                    BackkickForce = 2.5f,
+
+                    Trajectory = new AmmoTrajectory
+                    {
+                        Guidance = None,
+                        AccelPerSec = 0f,
+                        DesiredSpeed = 200f,
+                        MaxTrajectory = 800f,
+                        SpeedVariance = new Randomize {Start = 0, End = 10},
+                        RangeVariance = new Randomize {Start = 0, End = 100},
+                    },
+
+                    ShieldBehavior = new AmmoShieldBehavior
+                    {
+                        ShieldDmgMultiplier = 1.1f,
+                        ShieldDamage = Kinetic,
+                    },
+
+                },
+                GraphicDef = new GraphicDefinition
+                {
+                    ModelName = "",
+                    VisualProbability = 1f,
+                    ShieldHitDraw = true,
+
+                    Particles = new ParticleDefinition
+                    {
+                        AmmoParticle = "",
+                        AmmoColor = new Vector4(0, 0, 128, 32),
+                        AmmoOffset = new Vector3D(0, -1 , 0),
+                        AmmoScale = 1f,
+                        HitParticle = "",
+                        HitColor = new Vector4(0, 0, 0, 0),
+                        HitScale = 1f,
+                        Turret1Particle = "",
+                        Turret1Color = new Vector4(0, 0, 0, 0),
+                        Turret1Scale = 1f,
+                        Turret1Restart = false,
+                        Turret2Particle = "",
+                        Turret2Color = new Vector4(0, 0, 0, 0),
+                        Turret2Scale = 1f,
+                        Turret2Restart = true,
+                    },
+
+                    Line = new LineDefinition
+                    {
+                        Trail = true,
+                        Material = "ProjectileTrailLine", // WeaponLaser, WarpBubble, ProjectileTrailLine
+                        Color = new Vector4(10, 10, 10, 1),
+                        Width = 0.04f,
+                        RandomizeColor = new Randomize { Start =  1f, End = 2f },
+                        RandomizeWidth = new Randomize { Start =  0f, End = 0f },
+                    },
+                },
+                AudioDef = new AudioDefinition
+                {
+                    Turret = new AudioTuretDefinition
+                    {
+                        FiringSoundStart = "",
+                        FiringSoundLoop = "",
+                        FiringSoundEnd = "",
+                        FiringRange = 500f,
+                        FiringVolume = 1f,
+                        FiringPitchVar = new Randomize {Start = 0f, End = 0f},
+                        FiringVolumeVar = new Randomize {Start = 0f, End = 0f},
+                        ReloadSound = "",
+                        ReloadRange = 30f,
+                        ReloadVolume = 1f,
+                        NoAmmoSound = "",
+                        TurretRotationSound = "",
+                    },
+
+                    Ammo = new AudioAmmoDefinition
+                    {
+                        TravelSound = "",
+                        TravelRange = 350f,
+                        TravelVolume = 1f,
+                        TravelPitchVar = new Randomize {Start = 0, End = 0},
+                        TravelVolumeVar = new Randomize {Start = 0, End = 0},
+                        HitSound = "",
+                        HitRange = 450f,
+                        HitVolume = 1f,
+                        HitPitchVar = new Randomize {Start = 0, End = 0},
+                        HitVolumeVar = new Randomize {Start = 0, End = 0},
+                    },
+                },
+            },
 		// Don't edit below this line.
         };
     }
