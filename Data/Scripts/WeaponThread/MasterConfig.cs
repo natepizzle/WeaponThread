7354116c45b01af03d3570e666072d3fc3023fa1
Shane Stixrud <shane@geeklords.org>
2019-07-06 23:52:54 -0700

    dsadsa

--- Data/Scripts/WeaponThread/MasterConfig.cs
+++ Data/Scripts/WeaponThread/MasterConfig.cs
@@ -7,24 +7,29 @@ namespace WeaponThread
         internal Weapons(out WeaponDefinition[] weaponDefinitions)
         {
             // file convention: Name.cs
-            // add procedure: Weapon.Add(Name);
+            // add procedure: Add(Name1, Name2, Name3, etc..);
             // See Example.cs file for weapon property details.
-            // Add each of your WeaponName entries below
-            //
             // Don't edit above this line
 
-            Weapon.Add(Gatling);
-            Weapon.Add(Missile);
-            Weapon.Add(Torpedo);
+            Add(Missile, Torpedo, Gatling);
             
-            //
             // Don't edit below this line
-            //
-            weaponDefinitions = new WeaponDefinition[Weapon.Count];
-            for (int i = 0; i < Weapon.Count; i++) weaponDefinitions[i] = Weapon[i];
-            Weapon.Clear();
+            weaponDefinitions = ReturnDefs();
         }
 
+        // Don't edit below
         internal List<WeaponDefinition> Weapon = new List<WeaponDefinition>();
+        private void Add(params WeaponDefinition[] defs)
+        {
+            foreach (var def in defs) Weapon.Add(def);
+        }
+
+        private WeaponDefinition[] ReturnDefs()
+        {
+            var weaponDefinitions = new WeaponDefinition[Weapon.Count];
+            for (int i = 0; i < Weapon.Count; i++) weaponDefinitions[i] = Weapon[i];
+            Weapon.Clear();
+            return weaponDefinitions;
+        }
     }
 }
