25b819421c27129c12fb47b7cd4f27e36fbca956
Shane Stixrud <shane@geeklords.org>
2019-07-21 00:34:44 -0700

    dsadsa

--- Data/Scripts/WeaponThread/Missile.cs
+++ Data/Scripts/WeaponThread/Missile.cs
@@ -88,14 +88,14 @@ namespace WeaponThread
             RangeVariance = Random(start: 0, end: 0),
             Smarts = new Smarts
             {
-                Inaccuracy = 20f, // 0 is perfect, hit accuracy will be a random num of meters between 0 and this value.
+                Inaccuracy = 20f, // 0 = perfect, aim pos will be 0 - # meters from center, recalculates on miss.
                 Aggressiveness = 1f, // controls how responsive tracking is.
                 MaxLateralThrust = 0.5, // controls how sharp the trajectile may turn
                 TrackingDelay = 5, // Measured in line length units traveled.
                 MaxChaseTime = 1800, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                 TopTargets = 4, // 0 = unlimited, max number of targets to pick from
                 TopBlocks = 4, // 0 = unlimited, max number of blocks to pick from
-                OverideTarget = false, // when set to true ammo picks its own target, does not use hardpoints.
+                OverideTarget = false, // when set to true ammo picks its own target, does not use hardpoint's.
             },
         },
     },
