142ee24310b894eccd828e8baa8ca36ac8045a73
Shane Stixrud <shane@geeklords.org>
2019-08-16 20:56:51 -0700

    dsadsa

--- Data/Scripts/WeaponThread/GazerBeam.cs
+++ Data/Scripts/WeaponThread/GazerBeam.cs
@@ -68,6 +68,7 @@ namespace WeaponThread
             ClosestFirst = true, // targets closest of first subtarget until closest of next subtarget is reached, will switch back to previous subtarget if closer than next subtarget if set to true. If set to false will target and destroy all of subtarget groups and then move to next subtarget group.
             OnlyTargetSubSystems = false, //will not target other blocks if not in Priorities list
         },
+        MinimumRadius = 10, // 0 = unlimited, Minimum radius of threat to engage.
         TopTargets = 4, // 0 = unlimited, max number of top targets to randomize between.
         TopBlocks = 4, // 0 = unlimited, max number of blocks to randomize between
         StopTrackingSpeed = 70, // do not track target threats traveling faster than this speed
