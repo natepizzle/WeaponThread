bb02dc1dd7d479ff6f4f4e7f55b14cc53bb3009e
blitz2190 <blitz2190@DESKTOP-216274I>
2019-08-22 21:30:40 -0400

    asdasdasd

--- Data/Scripts/WeaponThread/DronePod.cs
+++ Data/Scripts/WeaponThread/DronePod.cs
@@ -93,7 +93,7 @@ namespace WeaponThread
                 Mass = 100f, // in kilograms
                 Health = 10000, // 0 = disabled, otherwise how much damage it can take from other trajectiles before dying.
                 BackKickForce = 0f,
-                ObjectsHit = Options(maxObjectsHit: 1, countBlocks: false), // 0 = disabled, value determines max objects (and/or blocks) penetrated per hit
+                ObjectsHit = Options(maxObjectsHit: 0, countBlocks: false), // 0 = disabled, value determines max objects (and/or blocks) penetrated per hit
                 Shrapnel = Options(baseDamage: 0, fragments: 0, maxTrajectory: 100, noAudioVisual: true, noGuidance: true, shape: HalfMoon),
 
                 AreaEffect = new AreaDamage
