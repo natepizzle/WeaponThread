96e10405a477369e926a293ad01fa59e8eb1b1b3
blitz2190 <blitz2190@DESKTOP-216274I>
2019-08-26 14:30:03 -0400

    asdasd

--- Data/Scripts/WeaponThread/DronePod.cs
+++ Data/Scripts/WeaponThread/DronePod.cs
@@ -17,7 +17,7 @@ namespace WeaponThread
             {
                 MountPoints = new[]
                 {
-                    MountPoint(subTypeId: "DronePod", aimPartId: "MissileTurretBarrels", muzzlePartId: "None"),
+                    MountPoint(subTypeId: "DronePod", aimPartId: "MissileTurretBarrels", muzzlePartId: "MissileTurretBarrels"),
                 },
                 Barrels = Names("muzzle_missile_001")
             },
