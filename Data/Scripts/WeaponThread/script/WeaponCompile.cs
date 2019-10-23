5337cf504ea933640d94a708e8e4b48a3e30f278
blitz2190 <blitz2190@DESKTOP-216274I>
2019-10-23 00:40:43 -0400

    asdasd

--- Data/Scripts/WeaponThread/script/WeaponCompile.cs
+++ Data/Scripts/WeaponThread/script/WeaponCompile.cs
@@ -128,7 +128,7 @@ namespace WeaponThread
             return new Session.TracerBaseDefinition { Enable = enable, Length = length, Width = width, Color = color};
         }
 
-        internal Session.AimControlDefinition AimControl(bool trackTargets, bool turretAttached, bool turretController, float rotateRate, float elevateRate, Vector3D offset, bool primaryTracking = false, int minAzimuth = 0, int maxAzimuth = 0, int minElevation = 0, int maxElevation = 0, bool fixedOffset = false, float inventorySize = .384f, bool debug = false)
+        internal Session.AimControlDefinition AimControl(bool trackTargets, bool turretAttached, bool turretController, bool primaryTracking, float rotateRate, float elevateRate, Vector3D offset, bool fixedOffset, float inventorySize, bool debug, int minAzimuth = -1, int maxAzimuth = -1, int minElevation = -1, int maxElevation = -1)
         {
             return new Session.AimControlDefinition { TrackTargets = trackTargets, TurretAttached = turretAttached, TurretController = turretController, RotateRate = rotateRate, ElevateRate = elevateRate, Offset = offset, Debug = debug, MinAzimuth = minAzimuth, MaxAzimuth = maxAzimuth, MinElevation = minElevation, MaxElevation = maxElevation, FixedOffset = fixedOffset, InventorySize = inventorySize, PrimaryTracking = primaryTracking };
         }
