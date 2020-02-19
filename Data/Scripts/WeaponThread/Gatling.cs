4e752fcf6085777956e74c495aa8537297d4e1f8
blitz2190 <blitz2190@DESKTOP-216274I>
2020-02-18 21:56:06 -0500

    asdasd

--- Data/Scripts/WeaponThread/Gatling.cs
+++ Data/Scripts/WeaponThread/Gatling.cs
@@ -57,6 +57,7 @@ namespace WeaponThread
                     DegradeRof = false, // progressively lower rate of fire after 80% heat threshold (80% of max heat)
                     ShotsInBurst = 0,
                     DelayAfterBurst = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    FireFullBurst = false,
                 },
             },
             Targeting = new TargetingDefinition
