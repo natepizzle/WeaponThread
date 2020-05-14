067877ece651a20e0ef45247c5431cb69cd3be7a
Shane Stixrud <shane@geeklords.org>
2020-05-13 22:30:50 -0700

    Changed MaxDuration to float

--- Data/Scripts/WeaponThread/script/WeaponStructure.cs
+++ Data/Scripts/WeaponThread/script/WeaponStructure.cs
@@ -656,7 +656,7 @@ namespace WeaponThread
             {
                 [ProtoMember(1)] internal float Scale;
                 [ProtoMember(2)] internal float MaxDistance;
-                [ProtoMember(3)] internal uint MaxDuration;
+                [ProtoMember(3)] internal float MaxDuration;
                 [ProtoMember(4)] internal bool Loop;
                 [ProtoMember(5)] internal bool Restart;
                 [ProtoMember(6)] internal float HitPlayChance;
